f8392ee16ba3d515809f92c3139fc3d580163c27
turabitbhumi-afk <turabit.bhumi@gmail.com>
2026-04-06 10:42:06 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,31 +1,3 @@
-# from playwright.sync_api import sync_playwright
-#
-# with sync_playwright() as p:
-#     browser = p.chromium.launch(headless=False)
-#     page = browser.new_page()
-#     page.goto("https://example.com")
-#
-#     print(page.title())
-#
-#     browser.close()
-
-
-# from playwright.sync_api import sync_playwright
-#
-# with sync_playwright() as p:
-#     browser = p.chromium.launch(headless=False)
-#     page = browser.new_page()
-#     page.goto("https://quotes.toscrape.com/")
-#
-#     page.wait_for_selector(".quote")
-#
-#     quotes = page.locator(".quote").all_text_contents()
-#     print(quotes)
-#
-#     browser.close()
-
-
-
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
